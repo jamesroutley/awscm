d92ba4b5470f6c7482d62c8d678052ec0c7f5a3e
James Routley <jroutley@gmail.com>
2017-05-08 13:50:58 +0100

    Fix install path

--- cmd/init.go
+++ cmd/init.go
@@ -3,18 +3,24 @@ package cmd
 import (
 	"fmt"
 	"github.com/spf13/cobra"
+	"log"
+	"os"
 )
 
 // init prints install instructions.
 func initialise(cmd *cobra.Command, args []string) {
-	msg := `# Install awscm by adding the following
+	executable, err := os.Executable()
+	if err != nil {
+		log.Fatalf("Could not locate executable: %v", err)
+	}
+	msg := fmt.Sprintf(`# Install awscm by adding the following
 # function to your shell startup script:
 
 awscm() {
     tmpfile=$(mktemp)
-    awscm-core --file "$tmpfile" "$@"
+    %s --file "$tmpfile" "$@"
     . "$tmpfile"
     rm "$tmpfile"
-}`
+}`, executable)
 	fmt.Println(msg)
 }
